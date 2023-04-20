88e1047ce410ffd511962fd1b10a1e57836d1cc4
Rayzr Reptile <padkins2020@fau.edu>
2023-04-20 16:32:11 -0400

    Details page touch-ups
    - Added button functionality to side tab sections
    - Adjusted general page view and inner contents
    - Added login popup functionality
    - Configured login popup for mobile (universal)
    - Removed preloader from details page

--- public/assets/js/details.js
+++ public/assets/js/details.js
@@ -14,6 +14,20 @@ function buildDetailsScreen() {
     image = `<img src="${retrievedCoverUrl}" alt="${retrievedGameName}" class="img-fluid cover-image"/>`;
     var coverSpot = document.getElementById("coverArt");
     coverSpot.insertAdjacentHTML("beforeend", image);
+
+    // Side tab buttons
+    let addToWishlist_Button = document.querySelector('.add-to-wishlist__button');
+    addToWishlist_Button.addEventListener("click", () => {
+        alert("Added to Wishlist [testing purposes]");
+    });
+    let addToBacklog_Button = document.querySelector('.add-to-backlog__button');
+    addToBacklog_Button.addEventListener("click", () => {
+        alert("Added to Backlog [testing purposes]");
+    });
+    let addToCustom_Button = document.querySelector('.add-to-custom__button');
+    addToCustom_Button.addEventListener("click", () => {
+        alert("Added to CustomList [testing purposes]");
+    });
 }
 
 window.onbeforeunload = function (e) {
