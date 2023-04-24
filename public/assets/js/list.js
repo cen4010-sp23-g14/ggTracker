eb93ea34efd4483461ffae02446b857159fd4e1c
Rayzr Reptile <padkins2020@fau.edu>
2023-04-24 18:31:20 -0400

    Finished adding create lists popup

--- public/assets/js/list.js
+++ public/assets/js/list.js
@@ -2,12 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
     function createListPopup() {
-        //let createListContainer = document.querySelector('.create-list__popup');
-        alert("pressed!");
-        return;
+        debugger;
+        let createListContainer = document.querySelector('.create-list__popup');
+        let listPopup = document.querySelector('.lists-popup');
+        let main = document.querySelector('main');
+        if (listPopup != null && !listPopup.classList.contains('hidden')) {
+            listPopup.classList.toggle('hidden');
+        }
+        if (!main.classList.contains('blur')) {
+            main.classList.toggle('blur');
+        }
+        createListContainer.classList.toggle('hidden');
     }
 
     let createListButton = document.querySelector('.create-custom-list__button');
     createListButton.addEventListener("click", createListPopup);
+
+    let createListExitButton = document.querySelector('.create-list__exit-button');
+    createListExitButton.addEventListener("click", () => {
+        let createListContainer = document.querySelector('.create-list__popup');
+        let main = document.querySelector('main');
+        createListContainer.classList.toggle('hidden');
+        main.classList.toggle('blur');
+    });
 
 });
