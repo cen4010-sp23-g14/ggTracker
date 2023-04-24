1bd57af0b4084bec52acd193c64f1779abcd48d0
Rayzr Reptile <padkins2020@fau.edu>
2023-04-24 16:48:51 -0400

    Preliminary code for adding create lists popup

--- public/assets/js/list.js
+++ public/assets/js/list.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
+    function createListPopup() {
+        //let createListContainer = document.querySelector('.create-list__popup');
+        alert("pressed!");
+        return;
+    }
+
+    let createListButton = document.querySelector('.create-custom-list__button');
+    createListButton.addEventListener("click", createListPopup);
+
 });
